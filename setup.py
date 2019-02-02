06f8b694d41278130a8e0a3222823b12c2512077
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-02-02 23:41:03 +0000

    Pip packaging working

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setuptools.setup(
     author_email="FalselyTrueBots@users.noreply.github.com",
     url="https://github.com/FalselyTrueBots",
     packages=["solonbot"],
-    #install_requires=["solon @ git+ssh://git@github.com/FalselyTrueBots/solon@ff88adea69f09dcb26ab72d4870f4939213ec7f8#egg=solon-0.0.1"]
+    install_requires=["solon @ git+ssh://git@github.com/FalselyTrueBots/solon@b1095c8bfefe2ca1294e52827bfa5d5d6443d4b2#egg=solon-0.0.1"]
 )
