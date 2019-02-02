609bd6f6fa826818d15786d3b73088dec745d040
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-02-02 23:51:54 +0000

    Clean up

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setuptools.setup(
     author_email="FalselyTrueBots@users.noreply.github.com",
     url="https://github.com/FalselyTrueBots",
     packages=["solonbot"],
-    install_requires=["solon @ git+ssh://git@github.com/FalselyTrueBots/solon@b1095c8bfefe2ca1294e52827bfa5d5d6443d4b2#egg=solon-0.0.1"]
+    install_requires=[
+        "solon @ git+ssh://git@github.com/FalselyTrueBots/solon@b1095c8bfefe2ca1294e52827bfa5d5d6443d4b2#egg=solon-0.0.1"
+    ]
 )
