bc6b42da025688e7695323d2ff3af7ace61b9880
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-02-03 20:46:22 +0000

    Activity based rankings

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="SolonBot",
-    version="0.0.2",
+    version="0.0.3",
     description="Discord bot for general server and community management. Uses Solon library.",
     author="Falsely True Bots",
     author_email="FalselyTrueBots@users.noreply.github.com",
     url="https://github.com/FalselyTrueBots",
     packages=["solonbot"],
     install_requires=[
-        "solon @ git+ssh://git@github.com/FalselyTrueBots/solon@b1095c8bfefe2ca1294e52827bfa5d5d6443d4b2#egg=solon-0.0.1"
+        "solon @ git+ssh://git@github.com/FalselyTrueBots/solon@3d4aa6e90ad13aab637761c8e321ebf365a300a6#egg=solon-0.0.2"
     ]
 )
--- solonbot/Activity.py
+++ solonbot/Activity.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 import logging
 import solon
@@ -12,6 +13,10 @@ log.info(f"Loading {__name__}")
 int_to_role_name = solon.SerializedDictionary(int, discord.Role).__name__
 
 default_settings = {
+    "ranking_message_delay": {"value_serialized": "2s", "type_name": "timedelta"},
+    "decay_interval": {"value_serialized": "20s", "type_name": "timedelta"},
+    "depreciation_factor": {"value_serialized": "0.96", "type_name": "float"},
+
     "award_eligible": {"value_serialized": "", "type_name": "role"},
     "award_ranks": {"value_serialized": "", "type_name": int_to_role_name},
     "award_method": {"value_serialized": "score", "type_name": "str"}
@@ -29,8 +34,10 @@ class Activity:
         self.guild_id = guild_id
         self.settings = settings
         self.data = data
+        self.last_message_sent = {}
 
         solon.register_scoreboard(self, guild_id, settings)
+        self.depreciate_all_scores.start(self, self.settings["decay_interval"])
 
     @property
     def scoreboard(self):
@@ -38,4 +45,23 @@ class Activity:
 
     @solon.Event()
     async def on_message(self, message):
-        pass
+        if not message.author.bot:
+            author_id = message.author.id
+            now = datetime.datetime.utcnow()
+            last_message_time = self.get_last_message_sent_time(author_id)
+            if now - last_message_time > self.settings["ranking_message_delay"]:
+                score = self.data.scoreboard.get(author_id, 0)
+                self.data.scoreboard[author_id] = score + 1
+                self.last_message_sent[author_id] = now
+
+    @solon.TimedEvent()
+    async def depreciate_all_scores(self):
+        factor = self.settings["depreciation_factor"]
+        for user_id, score in self.data.scoreboard.items():
+            self.data.scoreboard[user_id] = factor * score
+
+    def get_last_message_sent_time(self, author_id):
+        last_message_sent = self.last_message_sent.get(author_id)
+        if last_message_sent is None:
+            last_message_sent = datetime.datetime.min
+        return last_message_sent
