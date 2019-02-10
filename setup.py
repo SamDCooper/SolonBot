c11551eed7a66b7cb8558fa8dc43e12ec52ad1f7
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-02-10 22:45:55 +0000

    Bump to version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="SolonBot",
-    version="0.1.2",
+    version="0.1.3",
     description="Discord bot for general server and community management. Uses Solon library.",
     author="Falsely True Bots",
     author_email="FalselyTrueBots@users.noreply.github.com",
