452506ae0ffc5ceea7f4bb7bd2a451660ccf0bdf
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-03-16 18:22:02 +0000

    add global block chance to noreact

--- solonbot/Noreact.py
+++ solonbot/Noreact.py
@@ -1,5 +1,6 @@
 import discord
 import logging
+import random
 import solon
 
 __all__ = []
@@ -12,7 +13,8 @@ log.info(f"Loading {__name__}")
 member_list_name = solon.SerializedList(discord.Member).__name__
 
 default_settings = {
-    "blocked_users": {"value_serialized": "", "type_name": member_list_name}
+    "blocked_users": {"value_serialized": "", "type_name": member_list_name},
+    "block_chance": {"value_serialized": "1", "type_name": "float"}
 }
 
 
@@ -29,4 +31,7 @@ class NoReact:
             return
 
         if user in blocked_users:
-            await reaction.message.remove_reaction(reaction.emoji, user)
+            block_chance = self.settings["block_chance"]
+            block_chance = 1 if block_chance > 1 else 0 if block_chance < 0 else block_chance
+            if random.random() < block_chance:
+                await reaction.message.remove_reaction(reaction.emoji, user)
