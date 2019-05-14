5f78481101fd603b2d8a320a526f1c40f6796f7e
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-05-14 20:23:22 +0100

    Update to v21 and lib to v6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="SolonBot",
-    version="0.1.20",
+    version="0.1.21",
     description="Discord bot for general server and community management. Uses Solon library.",
     author="Falsely True Bots",
     author_email="FalselyTrueBots@users.noreply.github.com",
     url="https://github.com/FalselyTrueBots",
     packages=["solonbot"],
     install_requires=[
-        "solon @ git+ssh://git@github.com/FalselyTrueBots/solon@fe5e5b8b6d90dde1b255fa8cd3fdc40d3c41f6b3#egg=solon-0.0.5"
+        "solon @ git+ssh://git@github.com/FalselyTrueBots/solon@38821a416398a8d6a278974ddf2bf76efeacf667#egg=solon-0.0.6"
     ]
 )
