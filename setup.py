068d94ad2254b4bc7c6414f34c8fd47b8e6672b1
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-08-10 13:24:57 +0100

    Update to v23 and solon to v7

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="SolonBot",
-    version="0.1.22",
+    version="0.1.23",
     description="Discord bot for general server and community management. Uses Solon library.",
     author="Falsely True Bots",
     author_email="FalselyTrueBots@users.noreply.github.com",
     url="https://github.com/FalselyTrueBots",
     packages=["solonbot"],
     install_requires=[
-        "solon @ git+ssh://git@github.com/FalselyTrueBots/solon@38821a416398a8d6a278974ddf2bf76efeacf667#egg=solon-0.0.6"
+        "solon @ git+ssh://git@github.com/FalselyTrueBots/solon@de3c3dc4328f3a6ce89f87826ae8a3c4b7afa301#egg=solon-0.0.7"
     ]
 )
