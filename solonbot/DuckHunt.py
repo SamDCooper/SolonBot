2622fedc10f333f654c6ce41353e9d393ed497ac
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-08-26 18:36:23 +0100

    Bump version to 24

--- solonbot/DuckHunt.py
+++ solonbot/DuckHunt.py
@@ -17,6 +17,9 @@ int_to_role_name = solon.SerializedDictionary(int, discord.Role).__name__
 
 default_settings = {
     "duck_react": {"value_serialized": "🦆", "type_name": "Emoji"},
+    "golden_duck_react": {"value_serialized": "", "type_name": "Emoji"},
+    "golden_duck_points": {"value_serialized": "3", "type_name": "float"},
+    "golden_duck_chance": {"value_serialized": "0.2", "type_name": "float"},
     "can_play": {"value_serialized": "", "type_name": "role"},
     "spawn_rate": {"value_serialized": "0.0452", "type_name": "float"},
     "spawn_channels": {"value_serialized": "", "type_name": channel_list_name},
@@ -37,11 +40,13 @@ class Data:
 class DuckHunt:
     def __init__(self, guild_id, settings, data):
         self.chance_spawn_duck.start(self, solon.timedelta_from_string(config["chance_spawn_frequency"]))
-        self.current_duck = None
         self.spawn_duck = False
         self.data = data
         self.settings = settings
         self.guild_id = guild_id
+
+        self.current_duck = None
+        self.current_duck_golden = False
 
         solon.register_scoreboard(self, guild_id, settings)
 
@@ -52,10 +57,6 @@ class DuckHunt:
     @solon.TimedEvent()
     async def chance_spawn_duck(self):
         guild = solon.Bot.get_guild(self.guild_id)
-        if self.spawn_duck:
-            log.info(f"Duck was primed for spawning on {guild} but never spawned.")
-            self.spawn_duck = False
-            return
 
         spawn_rate = self.settings["spawn_rate"]
         spawn_chance = random.random()
@@ -71,9 +72,12 @@ class DuckHunt:
                 if duck_react is not None:
                     await self.current_duck.remove_reaction(duck_react.discord_py_emoji, solon.Bot.user)
                 self.current_duck = None
+                self.current_duck_golden = False
             else:
                 log.info(f"Time to spawn a duck on {guild}.")
                 self.spawn_duck = True
+        else:
+            self.spawn_duck = False
 
     @solon.Event()
     async def on_message(self, message):
@@ -91,6 +95,19 @@ class DuckHunt:
                 log.warning(f"duckhunt.duck_react is None on guild {message.guild}.")
                 return
 
+            golden_duck_react = self.settings["golden_duck_react"]
+            self.current_duck_golden = False
+            if golden_duck_react is not None:
+                golden_duck_chance = self.settings["golden_duck_chance"]
+                golden_duck_roll = random.random()
+                if golden_duck_roll < golden_duck_chance:
+                    self.current_duck_golden = True
+                    duck_react = golden_duck_react
+
+            guild = solon.Bot.get_guild(self.guild_id)
+            log.info(f"Spawning a duck in {message.channel} on {guild}.")
+            if self.current_duck_golden:
+                log.info(f"It's a golden duck!")
             await message.add_reaction(duck_react.discord_py_emoji)
             self.current_duck = message
 
@@ -98,14 +115,22 @@ class DuckHunt:
     async def on_reaction_add(self, reaction, user):
         if self.current_duck is not None and reaction.message.id == self.current_duck.id:
             if user.id != solon.Bot.user.id:
-                duck_react = self.settings["duck_react"]
+                if self.current_duck_golden:
+                    duck_react = self.settings["golden_duck_react"]
+                else:
+                    duck_react = self.settings["duck_react"]
+
                 if duck_react is not None and solon.emoji_equals(reaction.emoji, duck_react):
                     can_play = self.settings["can_play"]
                     if can_play is None or can_play in user.roles:
                         guild = solon.Bot.get_guild(self.guild_id)
                         log.info(f"{user} successfully got the duck on {guild}.")
                         score = self.data.scoreboard.get(user.id, 0)
-                        self.data.scoreboard[user.id] = score + 1
+
+                        additional = 1 if not self.current_duck_golden else self.settings["golden_duck_points"]
+                        self.data.scoreboard[user.id] = score + additional
+
                         await reaction.message.remove_reaction(reaction.emoji, user)
                         await reaction.message.remove_reaction(reaction.emoji, solon.Bot.user)
                         self.current_duck = None
+                        self.current_duck_golden = False
