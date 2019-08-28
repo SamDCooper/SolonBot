9c10c365baae7b2ca7411ad6ddbf8efe53428b22
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-08-28 19:38:34 +0100

    update to v26

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="SolonBot",
-    version="0.1.25",
+    version="0.1.26",
     description="Discord bot for general server and community management. Uses Solon library.",
     author="Falsely True Bots",
     author_email="FalselyTrueBots@users.noreply.github.com",
